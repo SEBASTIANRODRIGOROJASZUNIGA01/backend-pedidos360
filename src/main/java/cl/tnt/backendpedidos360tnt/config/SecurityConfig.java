4d898a580214347ce047b650db358516492663b4
Senavarrete2775 <se.navarretep@duocuc.cl>
2026-09-18 03:46:46 -0300

    feat: integración de MSAL, diseño de login y bypass temporal para pruebas locales

--- src/main/java/cl/tnt/backendpedidos360tnt/config/SecurityConfig.java
+++ src/main/java/cl/tnt/backendpedidos360tnt/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package cl.tnt.backendpedidos360tnt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,13 +16,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // Protege todas las rutas por defecto
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/pedidos").permitAll() //quitar solo para pruebas
                         .anyRequest().authenticated()
                 )
-                // Configura el Resource Server para validar el token JWT automáticamente
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> {})
+                        .jwt(Customizer.withDefaults())
                 );
 
         return http.build();
--- src/main/java/cl/tnt/backendpedidos360tnt/controller/PedidoController.java
+++ src/main/java/cl/tnt/backendpedidos360tnt/controller/PedidoController.java
@@ -1,18 +1,21 @@
 package cl.tnt.backendpedidos360tnt.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin(origins = "http://localhost:4200") // <---- temporal solo para prueba
 @RestController
 @RequestMapping("/api/pedidos")
 public class PedidoController {
 
-    // Spring Security antepone "SCOPE_" a los ámbitos leídos del token JWT
+    
     @GetMapping
-    @PreAuthorize("hasAuthority('SCOPE_OT.Create')")
+    //@PreAuthorize("isAuthenticated()")  <----- quitar el comentario para volver a la normalidad
     public String obtenerPedidos() {
-        return "¡Token válido y permiso OT.Create confirmado! Aquí están tus pedidos.";
+        return "¡Token válido y permiso OT.Create confirmado! Aquí están tus pedidos XDDD.";
     }
 }
