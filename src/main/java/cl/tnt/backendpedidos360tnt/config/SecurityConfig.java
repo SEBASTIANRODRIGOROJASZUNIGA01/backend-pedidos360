5d85cdb8394effb39c53a8997db4895ecfde0033
SEBASTIANRODRIGOROJASZUNIGA01 <zr.seba@gmail.com>
2026-09-22 17:04:57 -0300

    configuracion de rutas aws

--- src/main/java/cl/tnt/backendpedidos360tnt/config/SecurityConfig.java
+++ src/main/java/cl/tnt/backendpedidos360tnt/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package cl.tnt.backendpedidos360tnt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +17,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Permitir peticiones preflight OPTIONS
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
